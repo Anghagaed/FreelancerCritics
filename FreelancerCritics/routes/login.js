502ce9026afd5adf0439f83a9268a8b574f4a60f
paperplanes53 <paperplanes53@users.noreply.github.com>
2017-10-10 20:56:37 -0700

    Update login.js

--- FreelancerCritics/routes/login.js
+++ FreelancerCritics/routes/login.js
@@ -1,1 +1,24 @@
+var express = require('express');
+var router = express.Router();
 
+var LoginStatus;
+var RememberMe;
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+function Authenticate(username,password){
+    
+}
+
+function GotoProfilePage(){
+    
+}
+
+function DisplayStatus(){
+    
+}
