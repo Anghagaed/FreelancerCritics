1e0ef742c6152b039f071597d568fff340765331
paperplanes53 <paperplanes53@users.noreply.github.com>
2017-10-10 20:58:54 -0700

    Update searchbar.js

--- FreelancerCritics/routes/searchbar.js
+++ FreelancerCritics/routes/searchbar.js
@@ -1,1 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var accountType;
+var userID;
+
+
+function PrintResults(){
+    
+}
+
+function Search(userQuery){
+    
+}
+
+function UpdateQuery(){
+    
+}
+
+function DisplayError(){
+    
+}
 
