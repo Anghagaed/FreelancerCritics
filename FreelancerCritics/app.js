a7215e3529376f5170b0f664b840647ce343120b
Anghagaed <hliang5@ucmerced.edu>
2017-10-24 01:30:04 -0700

    Changes to app, index, and other. Read description
    Original App.js --> backup_app.js
    New app.js for me. Remove clutter and etc.
    Index.js --> changed so its just a router
    server.js --> empty just cause
    loginpage.html --> empty cause I don't have the original
    index.html --> add JS logic so it just redirects. Don't have to do on
    server side.

--- FreelancerCritics/app.js
+++ FreelancerCritics/app.js
@@ -1,53 +1,22 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-//var index = require('./public/javascripts/index');
-var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('routes', path.join(__dirname, 'routes'));
-//app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static('views'));
+app.use(express.static('routes'));
+app.use(express.static('public'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-//app.use('/public',express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+var start = require('./routes/index');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use('/', start);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var server = app.listen(4000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("FreelancerCritics app listening at http://%s:%s", host, port)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(3000);
+})
 
 module.exports = app;
--- FreelancerCritics/routes/index.js
+++ FreelancerCritics/routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+  res.sendFile(__dirname + '/views/index.html');
 });
-function func() {
-	router.redirect('redirect');
-}
 
 module.exports = router;
 
